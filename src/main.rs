406fe25e216e387209058f73c72a40daa59c7216
machida <machida@kmc.gr.jp>
2024-10-14 23:09:41 +0900

    BufReaderを使って書いてみる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+use std::io::BufRead;
+use std::path;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    // let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    let file = std::fs::File::open(&args.path).expect("could not read file");
+
+    let reader = std::io::BufReader::new(file);
+
+    for line in reader.lines() {
+        if let Ok(line) = line {
+            if line.contains(&args.pattern) {
+                println!("{}", line);
+            }
+        }
+    }
 }
